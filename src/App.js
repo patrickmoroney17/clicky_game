a94f00562c10d37de777e9715b80bf527fa32d60
patrickmoroney17 <patrickmoroney17@gmail.com>
2019-08-28 17:01:51 -0400

    images are appropriately sized now

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import friends from "./friends.json";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    clickedArr: [],
+    topScore: 0,
+    score: 0,
+    message: ""
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+
+  clickPicture = id => {
+
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+
+
   render() {
     return (
       <Wrapper>
